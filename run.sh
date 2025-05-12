ea49259b1f7dc84e6de1cbe27f5f141f4107c6ba
Harry Main <80337903+HarryCMain@users.noreply.github.com>
2025-05-12 13:57:43 +0100

    For VM

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This allows access to the host's camera devices
-docker run -it --rm \
-    --device=/dev/video0:/dev/video0 \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -e DISPLAY=$DISPLAY \
-    robot-race-tracker
